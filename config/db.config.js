8d8bd22a6b1910fac8bef20f131e5912096414ec
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-09-22 02:01:36 -0300

    Add DB Config

--- config/db.config.js
+++ config/db.config.js
@@ -1,1 +1,17 @@
-console.log('Hola soy el archivo de configuracion');
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    define: {
+      freezeTableName: true
+    }
+  });
+
+module.exports = connection;
