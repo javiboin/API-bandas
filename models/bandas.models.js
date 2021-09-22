f181e91ec98ffe307fb366b887452d16635d422a
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-09-22 02:14:17 -0300

    Add Bandas Models - first model

--- models/bandas.models.js
+++ models/bandas.models.js
@@ -1,1 +1,15 @@
-console.log('Estos son los models');
+const tablaModel = (connection, Sequelize) => {
+  const tabla = connection.define('tabla', {
+    campo: {
+      type: Sequelize.INTEGER,
+      primaryKey: true
+    },
+    otrocampo: {
+      type: Sequelize.STRING
+    }
+  },
+  {
+    timestamps: false
+  });
+  return Tabla
+}
