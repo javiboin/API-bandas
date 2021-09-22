4f77bc2bdd817302988ba79959492059de9d4616
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-09-22 00:35:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const { urlencoded } = require('body-parser');
 const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3028;
+
+app.use(express.urlencoded({ extended: true } ));
+app.use(express.json());
+
+const bandasRoutes = require('./routes/bandas.routes');
+
+app.use('/api/v1/bandas', bandasRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
